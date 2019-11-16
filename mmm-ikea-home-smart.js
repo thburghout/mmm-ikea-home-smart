276b71d7730ef24eebec5bb10c31068c1ed8023d
Thomas Burghout <thomas_burghout@hotmail.com>
2019-11-16 15:09:05 +0100

    convert to proper node_helper

--- mmm-ikea-home-smart.js
+++ mmm-ikea-home-smart.js
@@ -1,10 +1,18 @@
 
-Module.register("ikea-home-smart", {
+Module.register("mmm-ikea-home-smart", {
     defaults: {
         deviceMatchString: 'light',
     },
 
     start: function() {
+        Log.info("Starting module: " + this.name);
         this.sendSocketNotification('START', this.config);
+    },
+
+    notificationReceived: function(notification, payload) {
+        this.sendSocketNotification('HANDLE_NOTIFICATION', {
+            notification: notification,
+            payload: payload
+        });
     },
 });
--- node_helper.js
+++ node_helper.js
@@ -3,34 +3,49 @@ Tradfri = require("ikea-tradfri");
 
 module.exports = NodeHelper.create({
 
+    start: function() {
+        console.log("Starting" + this.name);
+        this.socketNotificationHandlers = {
+            "START": this.connectToTradfri.bind(this),
+            "HANDLE_NOTIFICATION": this.notificationReceived.bind(this),
+        };
+
+        this.notificationHandlers = {
+            'IKEA_HOME_SMART_SWITCH_LIGHTS': this.switchLights.bind(this),
+        };
+    },
+
     socketNotificationReceived: function(notification, payload) {
-        if (notification === "START") {
-            console.log(this.name + ": starting...");
-            this.config = payload;
-            this.tradfri = new Tradfri(this.config.host, this.config.identity);
-            this.tradfri.connect().then(this.onConnect).catch(console.error);
+        if (notification in this.socketNotificationHandlers) {
+            this.socketNotificationHandlers[notification](payload);
         }
+    },
+
+    connectToTradfri: function(config) {
+        console.log(this.name + ": starting...");
+        this.config = config;
+        this.tradfri = new Tradfri(this.config.host, this.config.identity);
+        this.tradfri.connect().then(this.onConnect.bind(this)).catch(console.error);
     },
 
     onConnect: function () {
         console.log(this.name  + ': Connected to IKEA Tradfri HUB');
     },
 
-    notificationReceived: function(notification, payload, sender) {
+    notificationReceived: function(payload) {
+        let notification = payload.notification;
+        payload = payload.payload;
         if (notification in this.notificationHandlers) {
             this.notificationHandlers[notification](payload);
         }
     },
 
-    notificationHandlers: {
-        'IKEA_HOME_SMART_SWITCH_LIGHTS': this.switchLights,
-    },
-
     switchLights: function(on) {
+        console.log(`${this.name} toggled lights ${on ? 'on' : 'off'} (${new Date(Date.now()).toLocaleString()})`);
         this.tradfri.devices.forEach(device => {
             if (device.name.toLowerCase().includes(this.config.deviceMatchString)) {
-                device.switch(on)
+                device.switch(on);
             }
         });
-    }
+    },
 });
